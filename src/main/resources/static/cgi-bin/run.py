b3d9ab0dd585a13ded7138fee4ee34fbe4619446
antoroesler <anton.roesler@stud.fra-aus.de>
2021-01-12 13:45:54 +0100

    cgi-test

---
+++ src/main/resources/static/cgi-bin/run.py
@@ -0,0 +1,8 @@
+# HTML is following
+print("Content-Type: text/html")
+# blank line, end of headers
+print()
+
+print("<title> This is a CGI script output</title>")
+print("<h1>This is my first CGI script</h1>")
+print("Hello, JavaTpoint!")  
