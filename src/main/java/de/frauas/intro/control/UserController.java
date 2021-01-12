59d85311f586c1b9318b7d09b2e369ac725fd8c2
antoroesler <anton.roesler@stud.fra-aus.de>
2021-01-12 17:47:30 +0100

    Added comments

--- src/main/java/de/frauas/intro/control/UserController.java
+++ src/main/java/de/frauas/intro/control/UserController.java
@@ -13,68 +13,67 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 import de.frauas.intro.DAO.GoogleBookAPI;
 import de.frauas.intro.DAO.UserDatabase;
 import de.frauas.intro.form.UserBookInfoForm;
 import de.frauas.intro.model.User;
 import de.frauas.intro.util.UriUtil;
 
+/**
+ * A Controller to handle HTTP requests for pages regarding the user. Like
+ * register, login or to view users.
+ * 
+ * @author Anton Roesler
+ *
+ */
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
 
-	
 	@Autowired
 	UserDatabase userDatabase;
 
-
-	
-	
-	@RequestMapping(value = "/login" ,method = RequestMethod.GET)
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginPage(Model model) {
 		User user = new User(); // User acts as a form.
 		model.addAttribute("loginForm", user);
 		return "user/login";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(Model model, @ModelAttribute("loginForm") User user) {
-		
 		if (userDatabase.checkUserPassword(user.getHash(), user.getPassword())) {
-			
 			String uri = "redirect:/my?" + UriUtil.addHeader("user", user.getHash());
 			return uri;
 		}
 		return "user/login";
 	}
 
-	@RequestMapping(value = "/register" ,method = RequestMethod.GET)
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String registerPage(Model model) {
 		User user = new User(); // User acts as a form.
 		model.addAttribute("loginForm", user);
 		return "user/register";
 	}
-	
+
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String register(Model model, @ModelAttribute("loginForm") User user, HttpServletResponse response) {
 		userDatabase.addUser(user);
 		return "redirect:/user/login";
 	}
-	
-	@RequestMapping(value = "/view" ,method = RequestMethod.GET)
-	public String viewUser(Model model, @RequestParam("user") String viewingUser, @RequestParam("search") String viewedUser) {
+
+	@RequestMapping(value = "/view", method = RequestMethod.GET)
+	public String viewUser(Model model, @RequestParam("user") String viewingUser,
+			@RequestParam("search") String viewedUser) {
 		User user = userDatabase.getUser(viewedUser);
-		System.out.println(viewedUser);
-		System.out.println(viewingUser);
-		System.out.println("^^^^^^^^^^^");
 		model.addAttribute("viewingUser", new UserBookInfoForm(viewingUser));
 		model.addAttribute("viewedUser", new UserBookInfoForm(viewedUser));
-		model.addAttribute("books", GoogleBookAPI.getAllBooksById(userDatabase.getBooksFromUser(viewedUser, UserBookCategory.OWNED)));
+		model.addAttribute("books",
+				GoogleBookAPI.getAllBooksById(userDatabase.getBooksFromUser(viewedUser, UserBookCategory.OWNED)));
 		model.addAttribute("user", user);
 		return "user/view";
 	}
-	
+
 	@RequestMapping(value = "/inquiry", method = RequestMethod.GET)
 	@ResponseBody
 	public String inquiryOld(Model model, @ModelAttribute("userHashForm") UserBookInfoForm infoForm) {
@@ -86,9 +85,10 @@ public class UserController {
 		String userHash = infoForm.getUser();
 		User user = userDatabase.getUser(userHash);
 		model.addAttribute("viewingUser", new UserBookInfoForm(userHash));
-		model.addAttribute("books", GoogleBookAPI.getAllBooksById(userDatabase.getBooksFromUser(userHash, UserBookCategory.OWNED)));
+		model.addAttribute("books",
+				GoogleBookAPI.getAllBooksById(userDatabase.getBooksFromUser(userHash, UserBookCategory.OWNED)));
 		model.addAttribute("user", user);
 		return "user/view";
 	}
-	
+
 }
--- src/main/java/de/frauas/intro/model/Book.java
+++ src/main/java/de/frauas/intro/model/Book.java
@@ -2,15 +2,30 @@ package de.frauas.intro.model;
 
 import java.net.URI;
 
+/**
+ * A POJO that represents a book. It is in the form to be used as a RestTamplete
+ * to get information form the GoogleBooksAPI. This is the reason why the book
+ * does not have an attribute title etc. Additional attributes that store
+ * information about the book are located with in the volumeInfo attribute, due
+ * to the structure of the GoogleBooksAPI JSON format.
+ * 
+ * @author Anton Roesler
+ *
+ */
 public class Book {
 
 	private String id;
-	private String kind;
 	private VolumeInfo volumeInfo;
 	private String autor;
 	private String shortTitle;
 	private URI imageUri;
 
+	/**
+	 * Volume info is used to store title, publisher, list of authors and more. This
+	 * is due to the structure of the GoogleBooksAPI JSON format.
+	 * 
+	 * @return an volumeInfo object.
+	 */
 	public VolumeInfo getVolumeInfo() {
 		return volumeInfo;
 	}
@@ -19,6 +34,11 @@ public class Book {
 		this.volumeInfo = volumeInfo;
 	}
 
+	/**
+	 * The id is the id given by GoogleBooks.
+	 * 
+	 * @return the id as a string.
+	 */
 	public String getId() {
 		return id;
 	}
@@ -27,29 +47,16 @@ public class Book {
 		this.id = id;
 	}
 
-	public String getKind() {
-		return kind;
-	}
-
-	public void setKind(String kind) {
-		this.kind = kind;
-	}
-
-	public String authors() {
-		String authorString = "";
-		
-		if (volumeInfo.getAuthors() != null) {
-		for (String aut : volumeInfo.getAuthors()) {
-			authorString += aut;
-			authorString += ", ";
-		}}
-		return authorString;
-	}
-
+	/**
+	 * This Method is important before using a book inside a thymeleaf template. The
+	 * book attribute autor and shortTitle are not included in the GoogleBooks JSON
+	 * objects. But they are required by the templates. setData() sets these
+	 * attributes to their appropriate values. It also makes sure that the title is
+	 * no longer than 35 characters and it only includes the first author.
+	 */
 	public void setData() {
 		if (volumeInfo.getAuthors() != null) {
 			if (volumeInfo.getAuthors().length != 0) {
-
 				this.autor = volumeInfo.getAuthors()[0];
 			}
 		}
@@ -61,14 +68,29 @@ public class Book {
 		}
 	}
 
+	/**
+	 * The imageURI is an URI for an thumbnail image file of the books cover. Given
+	 * by GoogleBooks.
+	 */
 	public void setImgUri() {
 		this.imageUri = volumeInfo.getImageLinks().get("thumbnail");
 	}
 
+	/**
+	 * The imageURI is an URI for an thumbnail image file of the books cover. Given
+	 * by GoogleBooks.
+	 * 
+	 * @return the URI of the thumbnail image.
+	 */
 	public URI getImageUri() {
 		return imageUri;
 	}
 
+	/**
+	 * The author is the first author of the book as specified by GoogleBooks.
+	 * 
+	 * @return the first author of the book.
+	 */
 	public String getAutor() {
 		return autor;
 	}
@@ -77,6 +99,12 @@ public class Book {
 		this.autor = autor;
 	}
 
+	/**
+	 * The shorttitle is a shortened version of the original title. Or the full title
+	 * if short enough. Default max length is 35 characters.
+	 * 
+	 * @return the short title as a string. 
+	 */
 	public String getShortTitle() {
 		return shortTitle;
 	}
