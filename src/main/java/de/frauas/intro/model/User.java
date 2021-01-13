a136b5acd86d9a4389fda2bde5a8803b8a29ad62
antoroesler <anton.roesler98@gmx.de>
2021-01-13 11:48:36 +0100

    Doc comments for the user class

--- src/main/java/de/frauas/intro/model/User.java
+++ src/main/java/de/frauas/intro/model/User.java
@@ -1,13 +1,20 @@
 package de.frauas.intro.model;
 
-import java.util.HashMap;
-
-
+/**
+ * A class to represent users of the application. Every user has an username and
+ * a password. A hash value can be calculated for every user. The hash value is
+ * used to store users in the database and to refer to a user in the web
+ * application.
+ * 
+ * Users can be stored in the user database.
+ * 
+ * @author Anton Roesler
+ *
+ */
 public class User {
 
 	private String username;
 	private String password;
-	
 
 	public User(String username, String password) {
 		super();
@@ -19,6 +26,15 @@ public class User {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Calculates a user specific hash value to simulate security.
+	 * 
+	 * The hash value depends on username and password. It's length is set to 7. The
+	 * length can be changed but only if all users in the database are deleted as
+	 * well. If the length is changed the existing users wont be recognized.
+	 * 
+	 * @return The calculated hash value as a string.
+	 */
 	public String getHash() {
 		String userString = username + password;
 		String hashCode = Integer.toHexString(userString.hashCode());
@@ -26,6 +42,7 @@ public class User {
 		return hashCode;
 	}
 
+	// Getters and Setters
 	public String getUsername() {
 		return username;
 	}
@@ -42,17 +59,23 @@ public class User {
 		this.password = password;
 	}
 
-	
-	public HashMap<String, String> toHashMap() {
-		HashMap<String, String> hashMap = new HashMap<>();
-		hashMap.put("username", username);
-		hashMap.put("password", password);
-		return hashMap;
-	}
+//	public HashMap<String, String> toHashMap() {
+//		HashMap<String, String> hashMap = new HashMap<>();
+//		hashMap.put("username", username);
+//		hashMap.put("password", password);
+//		return hashMap;
+//	}
 
+	/**
+	 * Method to create a user in JSON format. Including: username, password and the
+	 * hash value.
+	 * 
+	 * @return A string in JSON format, with the user's information.
+	 */
 	public String toJSON() {
-		return "{ \"username\":\""+username+"\", \"password\":\""+password+"\" , \"hash\":\""+getHash()+"\" }";
-		
+		return "{ \"username\":\"" + username + "\", \"password\":\"" + password + "\" , \"hash\":\"" + getHash()
+				+ "\" }";
+
 	}
 
 }
