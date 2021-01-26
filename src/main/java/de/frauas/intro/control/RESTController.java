65cab7b5ac1d30c1baeef7b12692f680a960328f
antoroesler <anton.roesler98@gmx.de>
2021-01-26 15:38:25 +0100

    Improved some RestController responses

--- src/main/java/de/frauas/intro/control/RESTController.java
+++ src/main/java/de/frauas/intro/control/RESTController.java
@@ -1,9 +1,9 @@
 package de.frauas.intro.control;
 
-import java.io.File;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,21 +32,15 @@ public class RESTController {
 	@Autowired
 	UserDatabase userDatabase;
 
-	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
-	@ResponseBody
-	public String getUser() {
-
-		return "WELCOME";
-
-	}
-
 	@RequestMapping(value = "/user", method = RequestMethod.POST, consumes = { "application/JSON" })
 	@ResponseBody
 	public ResponseEntity<String> addUser(@RequestBody User user, @RequestHeader("Authorization") String auth) {
 		if (isValidAuthentication(auth)) {
-			if (userDatabase.addUser(user))
-				return new ResponseEntity<>(HttpStatus.CREATED);
-			else
+			if (userDatabase.addUser(user)) {
+				HttpHeaders headers = new HttpHeaders();
+				headers.add("Location", "/user/" + user.getUsername());
+				return new ResponseEntity<>(headers, HttpStatus.CREATED);
+			} else
 				return new ResponseEntity<String>(HttpStatus.CONFLICT);
 		}
 		return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
@@ -110,31 +104,35 @@ public class RESTController {
 		// TODO: refactor!
 		String body = "";
 		if (isValidAuthentication(auth)) {
-			ArrayList<String> books = userDatabase.getBooksFromUser(username, UserBookCategory.OWNED);
-			body = "{\"books_owned\":[";
-			for (int i = 0; i < books.size(); i++) {
-				String string = books.get(i);
-				body += "\"";
-				body += string;
-				body += "\"";
-				if (books.size() > i) {
-					body += ",";
+			if (userDatabase.getUser(username) != null) {
+				ArrayList<String> books = userDatabase.getBooksFromUser(username, UserBookCategory.OWNED);
+				body = "{\"books_owned\":[";
+				for (int i = 0; i < books.size(); i++) {
+					String string = books.get(i);
+					body += "\"";
+					body += string;
+					body += "\"";
+					if (books.size() > i) {
+						body += ",";
+					}
 				}
-			}
-			body += "],";
-			ArrayList<String> books2 = userDatabase.getBooksFromUser(username, UserBookCategory.WANTED);
-			body += "\"books_wanted\":[";
-			for (int i = 0; i < books2.size(); i++) {
-				String string = books2.get(i);
-				body += "\"";
-				body += string;
-				body += "\"";
-				if (books2.size() > i) {
-					body += ",";
+				body += "],";
+				ArrayList<String> books2 = userDatabase.getBooksFromUser(username, UserBookCategory.WANTED);
+				body += "\"books_wanted\":[";
+				for (int i = 0; i < books2.size(); i++) {
+					String string = books2.get(i);
+					body += "\"";
+					body += string;
+					body += "\"";
+					if (books2.size() > i) {
+						body += ",";
+					}
 				}
+				body += "]}";
+				return new ResponseEntity<>(body, HttpStatus.OK);
 			}
-			body += "]}";
-			return new ResponseEntity<>(body, HttpStatus.OK);
+			else
+				return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 	}
